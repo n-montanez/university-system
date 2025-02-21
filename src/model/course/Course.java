27b697c4f0e7a0f011d3711f1ea0132976971e20
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-02-21 13:23:48 -0500

    feat: adding course model

--- src/model/course/Course.java
+++ src/model/course/Course.java
@@ -1,4 +1,55 @@
 package model.course;
 
+import model.student.Student;
+import model.teacher.Teacher;
+
+import java.util.List;
+
 public class Course {
+    private String name;
+    private String classroom;
+    private List<Student> studentList;
+    private Teacher teacher;
+
+    public Course() {
+    }
+
+    public Course(String name, String classroom, List<Student> studentList, Teacher teacher) {
+        this.name = name;
+        this.classroom = classroom;
+        this.studentList = studentList;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
